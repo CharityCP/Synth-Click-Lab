4a741122eb15f4d94ec63b06b37c51968c491275
CharityCP <perkinscharity78@gmail.com>
2026-08-20 11:41:27 -0500

    update new content

---
+++ script.js
@@ -0,0 +1,29 @@
+let headline = document.getElementById("headline-1");
+let synth1 = document.getElementById("synth-1");
+let button1 = document.getElementById("synth-1");
+
+button1.addEventListener("click", function(){
+	synth1.textContent = "What faction does Synth lead?";
+});
+
+let kim1 = document.getElementById("kim-1");
+let button2 = document.getElementById("kim-1");
+
+button2.addEventListener("click", function(){
+	kim1.textContent = "Why did Jae Hyuk join the Rebellion?";
+});
+
+let aridyn1 = document.getElementById("aridyn-1");
+let button3 = document.getElementById("aridyn-1");
+
+button3.addEventListener("click", function(){
+	aridyn1.textContent = "Which side of the rebellion is Aridyn on?";
+});
+
+let kryos1 = document.getElementById("kryos-1");
+let button4 = document.getElementById("kryos-1");
+
+button4.addEventListener("click", function(){
+	kryos1.textContent = "Why does Kryos oppose humanity?";
+});
+
