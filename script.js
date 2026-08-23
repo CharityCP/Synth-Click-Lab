bb7875547534639c14025b1e892d55c3dea48d1c
CharityCP <perkinscharity78@gmail.com>
2026-08-23 17:07:24 -0500

    troubleshoot button in JavaScript

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
 let headline = document.getElementById("headline-1");
 	
 	
-let synth1= document.getElementById("synthInputBox).value;
-let button1= "synth-1";
+let button1= document.getElementById("synth-1");
 let correctAnswer1= "The Unity Nexus";
 
 button1.addEventListener("click", function(){
+	let synth1= document.getElementById("synthInputBox").value;
 
-if (synth1=== correctAnswer) {
-	results.innerHTML=
-	'<img src="award.jpg" alt="reward emblem>'
-	width="80">You got the answer correct!'
-	<p> Reward UNLOCKED!!</p>
+
+if (synth1=== correctAnswer1) {
+	console.log("correctAnswer");
+	results.innerHTML='<img src="award1.jpg" alt="reward emblem" width="80">You got the answer correct!<p> Reward UNLOCKED!!</p>'
 	results.className= "reward";
 	
 }else{ 
-results.innerHTML=
-<h2> OOPS Try Again!</h2>
-<p>Keep Practicing and play again!</p>
+console.log("not correctAnswer");
+results.innerHTML='<h2> OOPS Try Again!</h2><p>Keep Practicing and play again!</p>'
 results.className= "OOPS";
-
-
+}
+});
 	
 
 // Display Results
-const results= document.getElementById(results);
+//const results= document.getElementById(results);
 	
 
