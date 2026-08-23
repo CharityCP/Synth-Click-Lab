bb57f35f959aaed4e567859ce61b9af5df586a99
CharityCP <perkinscharity78@gmail.com>
2026-08-22 21:23:59 -0500

    add new updates

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let headline = document.getElementById("headline-1");
 	
 	
-let synth1= document.getElementById("synth-1").value;
+let synth1= document.getElementById("synthInputBox).value;
+let button1= "synth-1";
 let correctAnswer1= "The Unity Nexus";
+
+button1.addEventListener("click", function(){
 
 if (synth1=== correctAnswer) {
 	results.innerHTML=
-	'<img src="award.jpg" alt="reward emblem"
+	'<img src="award.jpg" alt="reward emblem>'
 	width="80">You got the answer correct!'
 	<p> Reward UNLOCKED!!</p>
 	results.className= "reward";
@@ -14,10 +17,10 @@ if (synth1=== correctAnswer) {
 }else{ 
 results.innerHTML=
 <h2> OOPS Try Again!</h2>
-<p>Keep Practicing and try again!</p>
+<p>Keep Practicing and play again!</p>
 results.className= "OOPS";
 
-button1.addEventListener("click", function(){
+
 	
 
 // Display Results
