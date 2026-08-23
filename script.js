03608c3485f113a8742c7ebd8f4ce9e2f0d768dd
CharityCP <perkinscharity78@gmail.com>
2026-08-22 20:19:36 -0500

    update files and scorecard, closes #7

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
 let headline = document.getElementById("headline-1");
-let synth1 = document.getElementById("synth-1");
-let button1 = document.getElementById("synth-1");
+	
+	
+let synth1= document.getElementById("synth-1").value;
+let correctAnswer1= "The Unity Nexus";
+
+if (synth1=== correctAnswer) {
+	results.innerHTML=
+	'<img src="award.jpg" alt="reward emblem"
+	width="80">You got the answer correct!'
+	<p> Reward UNLOCKED!!</p>
+	results.className= "reward";
+	
+}else{ 
+results.innerHTML=
+<h2> OOPS Try Again!</h2>
+<p>Keep Practicing and try again!</p>
+results.className= "OOPS";
 
 button1.addEventListener("click", function(){
-	synth1.textContent = "What faction does Synth lead?";
-});
+	
 
-let kim1 = document.getElementById("kim-1");
-let button2 = document.getElementById("kim-1");
-
-button2.addEventListener("click", function(){
-	kim1.textContent = "Why did Jae Hyuk join the Rebellion?";
-});
-
-let aridyn1 = document.getElementById("aridyn-1");
-let button3 = document.getElementById("aridyn-1");
-
-button3.addEventListener("click", function(){
-	aridyn1.textContent = "Which side of the rebellion is Aridyn on?";
-});
-
-let kryos1 = document.getElementById("kryos-1");
-let button4 = document.getElementById("kryos-1");
-
-button4.addEventListener("click", function(){
-	kryos1.textContent = "Why does Kryos oppose humanity?";
-});
+// Display Results
+const results= document.getElementById(results);
+	
 
