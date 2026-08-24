d12de64a7ac8a59953180efde5dc734d1d5efc31
CharityCP <perkinscharity78@gmail.com>
2026-08-23 20:41:28 -0500

    finished adding events for quiz responses, close #3

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let headline = document.getElementById("headline-1");
-	
-	
 let button1= document.getElementById("synth-1");
 let correctAnswer1= "The Unity Nexus";
 
@@ -15,13 +12,66 @@ if (synth1=== correctAnswer1) {
 	
 }else{ 
 console.log("not correctAnswer");
-results.innerHTML='<h2> OOPS Try Again!</h2><p>Keep Practicing and play again!</p>'
+results.innerHTML='<h2> OOPS Try Again!</h2><p> Correct answer is: The Unity Nexus</p><p>Keep Practicing and play again!</p>'
 results.className= "OOPS";
 }
 });
 	
 
-// Display Results
-//const results= document.getElementById(results);
+let button2= document.getElementById("kim-1");
+let correctAnswer2= "Betrayed by his father";
+
+button2.addEventListener("click", function () {
+	let kim1= document.getElementById("kimInputBox").value;
+	
+	
+if (kim1=== correctAnswer2) {
+console.log("correctAnswer");
+	results.innerHTML='<img src="award1.jpg" alt="reward emblem" width="80">You got the answer correct!<p> Reward UNLOCKED!!</p>'
+	results.className= "reward";
+	
+}else{ 
+console.log("not correctAnswer");
+results.innerHTML='<h2> OOPS Try Again!</h2><p> Correct answer is: Betrayed by his father</p><p>Keep Practicing and play again!</p>'
+results.className= "OOPS";
+}
+});
 	
 
+let button3= document.getElementById("aridyn-1");
+let correctAnswer3= "Self Preservation";
+
+button3.addEventListener("click", function () {
+	let aridyn1= document.getElementById("aridynInputBox").value;
+
+if (aridyn1=== correctAnswer3) {
+console.log("correctAnswer");
+	results.innerHTML='<img src="award1.jpg" alt="reward emblem" width="80">You got the answer correct!<p> Reward UNLOCKED!!</p>'
+	results.className= "reward";
+	
+}else{ 
+console.log("not correctAnswer");
+results.innerHTML='<h2> OOPS Try Again!</h2><p> Correct answer is: Self Preservation</p><p>Keep Practicing and play again!</p>'
+results.className= "OOPS";
+}
+});	
+
+
+let button4= document.getElementById("kryos-1");
+let correctAnswer4= "Humanity and their ways";
+
+button4.addEventListener("click", function (){
+	let kryos1= document.getElementById("kryosInputBox").value;
+	
+if (kryos1 === correctAnswer4) {
+console.log("correctAnswer");
+	results.innerHTML='<img src="award1.jpg" alt="reward emblem" width="80">You got the answer correct!<p> Reward UNLOCKED!!</p>'
+	results.className= "reward";
+	
+}else{ 
+console.log("not correctAnswer");
+results.innerHTML='<h2> OOPS Try Again!</h2><p> Correct answer is: Humanity and their ways</p><p>Keep Practicing and play again!</p>'
+results.className= "OOPS";
+}
+});		
+	
