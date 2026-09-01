13cfff5dd3cb4002d61d304bdee0a4f7369838d4
CharityCP <perkinscharity78@gmail.com>
2026-08-31 21:37:04 -0500

    update score tabulation, closes #5

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 let button1= document.getElementById("synth-1");
 let correctAnswer1= "The Unity Nexus";
+let scoreBoard1 = document.getElementById("scoreBoard1");
+let score = 0;
 
 button1.addEventListener("click", function(){
 	let synth1= document.getElementById("synthInputBox").value;
+	
+	
+	
 	
 	
 	if (synth1=== correctAnswer1) {
 		console.log("correctAnswer");
 		results.innerHTML='<img src="award1.jpg" alt="reward emblem" width="100">You got the answer correct!<p> Reward UNLOCKED!!</p>'
 		results.className= "reward";
+		score = score +3;
+		scoreBoard1.innerHTML = score;
+		
 		
 		}else{ 
 		console.log("not correctAnswer");
 		results.innerHTML='<h2> OOPS Try Again!</h2><p> Correct answer is: The Unity Nexus</p><p>Keep Practicing and play again!</p>'
 		results.className= "OOPS";
+		score = score -1;
+		scoreBoard1.innerHTML = score;
+		
 	}
 });
 
